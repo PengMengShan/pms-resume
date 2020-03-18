912406cab70a5dca4c57ec4e45c0bf042dd85640
pms <pengmengshan@163.com>
2020-03-18 13:26:52 +0800

    Add files via upload

---
+++ js/accordion_data.js
@@ -0,0 +1,81 @@
+var horiObj = {},
+   varObj = {};
+horiObj.imageObj = [
+
+   {
+      "src": "images/02.jpg",
+      "company": "上海摩库数据技术有限公司<br>2017/8-至今",
+      "url": "http://www.molbase.cn/",
+      "title": "上海摩库数据技术有限公司",
+      "icon": "fa-dashboard",
+      "content": "2017/8-至今<br>前端工程师<br>工作描述： <br>1、负责前台网站页面开发和维护，前台网站主要分为：摩贝主站（拥有4500万化合物数据信息：搜索，展示，维护。） 摩贝商城（化学品阐述，购买，支付，售后。）<br>2、负责微信公众号摩库数据页面管理和开发<br>3、负责微信小程序项目（摩贝云店、摩贝化工）搭建和开发<br>4、负责sass平台管理后台开发，数据可视化开发<br>5、协调前端人员开发完成项目-开发-测试-验收-发布。"
+   }, {
+      "src": "images/03.jpg",
+      "company": "江苏优兑网络科技有限公司上海分公司<br>2016/4 – 2017/8<br> ",
+      "url": "http://www.udui.com/",
+      "title": "江苏优兑网络科技有限公司上海分公司",
+      "icon": "fa-globe",
+      "content": '2016/4 – 2017/8<br>前端工程师<br>工作描述： <br>1、负责Web及移动端产品的前端页面开发使用HTML5、CSS3、Js等前端技术进行产品前端的开发。<br> 2、 使用React、 Vue等新的主流框架， 维护前端框架的技术升级。<br>3、 对微信端的优兑商城进行部分重构， 利用一些基于Node.JS的项目构建工具对项目进行配置（ webpack,gulp）<br>4、 负责Web前端系统和功能调试和维护。<br>5、 配合产品经理和UI设计师， 研究并改善用户体验。<br>6、 实现前后端的数据交互。'
+   }, {
+      "src": "images/04.jpg",
+      "company": "上海安师傅汽车服务股份有限公司<br>2014/07 -- 2016/4<br>",
+      "url": "http://www.4001002003.com/",
+      "title": "上海安师傅汽车服务股份有限公司",
+      "icon": "fa-tasks",
+      "content": "2014/07 -- 2016/4<br>前端工程师<br>工作描述： <br>1、使用Div+Css以及Html5+Css3等技术开发符合W3C标准的前端Pc页面及响应式页面。<br>2、使用Javascript 、Jquery、Ajax等技术完成页面美化交互和数据交互。<br>3、根据产品需求和平面设计，优化后期前端用户体现效果，提高用户体验。<br>4、解决 Pc 端网页的兼容问题，及移动端的适配问题。<br>5、使用Firefox、Chrome等浏览器的Debug工具并快速解决页面中出现的Bug。"
+   }, {
+      "src": "images/05.jpg",
+      "company": "项目经验<br>SaaS后台管理系统",
+      "url": "http://saas.molbase.cn/",
+      "title": "SaaS后台管理系统",
+      "icon": "fa-rss-square",
+      "content": "SaaS平台管理系统为化工业供应商企业，企业管理员及其员工实现以下功能来管理企业及企业店铺的业务。 功能部分：线索管理、客户管理、公海、采购需求、店铺管理、销售订单、发货单、回款单、开票申请、退款单、消息管理、消息配置、账户管理、部门管理、岗位管理、权限管理功能，实现SaaS平台统一账户体系。<br>个人职责：使用vue+element-ui+webpack搭建SaaS前端框架，实现权限管理，公共代码封装，前端项目管理及工作协调。"
+   }, {
+      "src": "images/06.jpg",
+      "company": "项目经验<br>微信小程开发<br>其他经验详细看纸质版简历",
+      "url": "http://www.pengmengshan.cn/",
+      "title": "摩贝云店(微信小程序)",
+      "icon": "fa-th-large",
+      "content": "摩贝云店主要依托微信端进行生态一体化营销的化学品小程序店铺。通过公众号推广、朋友圈扩散、微信群转发、小程序分享、帮实现高效的客户营销，推广获客，成交转化，客户留存，增购复购，分享裂变，营销迭代等一系列综合服务的掌上店铺。<br>个人职责：使用Mpvue+vuex+weui+vant搭建小程序架构，单独管理和开发及发布。"
+   },
+
+];
+
+
+varObj.imageObj = [{
+   "src": "images/ver-1.jpg",
+   "url": "https://www.baidu.com",
+   "title": "Mercedes-Benz",
+   "icon": "fa-plane",
+   "content": "The German car brand Mercedes Benz, a hundred years of civilization, automobile pioneer and leader"
+}, {
+   "src": "images/ver-2.jpg",
+   "url": "",
+   "title": "Real estate",
+   "icon": "fa-dashboard",
+   "content": "Real estate is called real estate in economics because of its own characteristics"
+}, {
+   "src": "images/ver-3.jpg",
+   "url": "https://www.baidu.com",
+   "title": "Tourism",
+   "icon": "fa-globe",
+   "content": '"Trip" travel, go out, in order to achieve a goal in space from a to B in the process;'
+}, {
+   "src": "images/ver-4.jpg",
+   "url": "",
+   "title": "Basketball",
+   "icon": "fa-tasks",
+   "content": "NBA（National Basketball Association）is the abbreviation of the American men's professional basketball league."
+}, {
+   "src": "images/ver-5.jpg",
+   "url": "",
+   "title": "description",
+   "icon": "fa-rss-square",
+   "content": "NBA（National Basketball Association）is the abbreviation of the American men's professional basketball league."
+}, {
+   "src": "images/ver-6.jpg",
+   "url": "",
+   "title": "Real estate",
+   "icon": "fa-th-large",
+   "content": "Real estate is called real estate in economics because of its own characteristics, that is, the immobility and immobility of position. There are three forms of existence: land, buildings and premises. real estate has become a major component of commercial transactions."
+}];
